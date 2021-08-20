355588353b73a2b36faab2cffd60a9126729fba4
Vinay288 <vinayhiremath288@gmail.com>
2021-08-20 09:58:10 +0530

    added wage calc per day feature

--- src/EmpWage.java
+++ src/EmpWage.java
@@ -1,7 +1,15 @@
 
 public class EmpWage {
-	public static void main(String [] args) {
+	public static void main(String[] args) {
 		System.out.println("*******welcome to emp wage calculator");
+		
+		int attendence = (int) Math.floor(Math.random() * 10) % 2;
+		
+		final int WAGE_PER_HOUR = 20;
+		final int HOUR_PER_DAY = 8;
+		
+		int wage = WAGE_PER_HOUR * HOUR_PER_DAY * attendence;
+		System.out.println("emp wage is "+wage);
 	}
 
 }
