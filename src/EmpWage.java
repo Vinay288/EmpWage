e0c27d1c84dae03f81b2d4bcc659a8df8416197b
Vinay288 <vinayhiremath288@gmail.com>
2021-08-20 10:05:22 +0530

    added part time to wage

--- src/EmpWage.java
+++ src/EmpWage.java
@@ -1,7 +1,15 @@
 
 public class EmpWage {
-	public static void main(String [] args) {
+	public static void main(String[] args) {
 		System.out.println("*******welcome to emp wage calculator");
+		int attendence = (int) Math.floor((Math.random() * 10) % 3);
+
+		final int WAGE_PER_HOUR = 20;
+		final int HOUR_FULLTIME = 8;
+		final int HOUR_PART_TIME = 4;
+
+		int wage = WAGE_PER_HOUR * HOUR_PART_TIME * attendence;
+		System.out.println("emp wage is " + wage);
 	}
 
 }
