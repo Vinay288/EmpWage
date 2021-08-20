ea79093a9a4c7d7db27daa39f5244d5130c965e8
Vinay288 <vinayhiremath288@gmail.com>
2021-08-20 10:19:31 +0530

    added wage calc for month

--- src/EmpWage.java
+++ src/EmpWage.java
@@ -2,13 +2,13 @@
 public class EmpWage {
 	public static void main(String[] args) {
 		System.out.println("*******welcome to emp wage calculator");
-		int attendence = (int) Math.floor((Math.random() * 10) % 3);
-
+		
 		final int WAGE_PER_HOUR = 20;
 		final int HOUR_FULLTIME = 8;
 		final int HOUR_PART_TIME = 4;
-		int workedHour = 0;
-
+		int workedHour = 0,totalWage=0;
+for(int i=0;i<20;i++) {
+	int attendence = (int) Math.floor((Math.random() * 10) % 3);
 		switch (attendence) {
 		case 0:
 			workedHour = 0;
@@ -22,8 +22,9 @@ public class EmpWage {
 		default:
 			System.out.println("error");
 		}
-		int wage = WAGE_PER_HOUR * workedHour * attendence;
-		System.out.println("emp wage is " + wage);
+		totalWage =totalWage + WAGE_PER_HOUR * workedHour * attendence;
+		
 	}
-
+System.out.println("emp wage is " + totalWage);
+}
 }
