22dac5506acf6b9ad034e00121896c282a304576
Vinay288 <vinayhiremath288@gmail.com>
2021-08-21 22:31:25 +0530

    Update EmpWage.java

--- src/EmpWage.java
+++ src/EmpWage.java
@@ -1,6 +1,12 @@
 
 public class EmpWage {
 	public static void main(String [] args) {
+		
+		
+		
+		
+		
+		
 		System.out.println("*******welcome to emp wage calculator*************");
 		int attendence = (int) Math.floor(Math.random()*10)%2;
 		if(attendence==1)
