d1131511c22765074af9c1a8b4c7566c7ee052bd
Vinay288 <vinayhiremath288@gmail.com>
2021-08-26 22:25:11 +0530

    ADDED: feature calculate wage for multiple companies

--- src/EmpWage.java
+++ src/EmpWage.java
@@ -2,7 +2,9 @@
 public class EmpWage {
 	public static void main(String[] args) {
 		System.out.println("*******welcome to emp wage calculator");
-		WageComputation wage=new WageComputation();
+		WageComputation wage=new WageComputation(20,10,4,"1");
+		WageComputation wage2=new WageComputation(50, 12, 6,"2");
 		wage.calculateWage();
+		wage2.calculateWage();
 	}
 }
--- src/WageComputation.java
+++ src/WageComputation.java
@@ -1,32 +1,68 @@
 
 public class WageComputation {
-	private final int WAGE_PER_HOUR = 20;
-	private final int HOUR_FULLTIME = 8;
-	private final int HOUR_PART_TIME = 4;
-public void calculateWage() {
-	int workedHour = 0, totalWage = 0,totalWorkingHour=0,totalDays=0;
-		while(totalWorkingHour<100 && totalDays<20) {
-		int attendence = attendence();
-		switch (attendence) {
-		case 0:
-			workedHour = 0;
-			break;
-		case 1:
-			workedHour = HOUR_FULLTIME;
-			break;
-		case 2:
-			workedHour = HOUR_PART_TIME;
-			break;
-		default:
-			System.out.println("error");
-		}
-		totalWorkingHour=totalWorkingHour+workedHour;
-		totalDays++;
-		totalWage = totalWage + WAGE_PER_HOUR * workedHour * attendence;
+	private int wagePerHour;
+	private int fullTimeHour;
+	private int partTimeHour;
+	private String companyName;
+
+	WageComputation(int wagePerHour, int fullTimeHour, int partTimeHour, String companyName) {
+
+		this.wagePerHour = wagePerHour;
+		this.fullTimeHour = fullTimeHour;
+		this.partTimeHour = partTimeHour;
+		this.companyName = companyName;
 	}
-	System.out.println("emp wage is " + totalWage);
-}
-public int attendence() {
-	return (int) Math.floor((Math.random() * 10) % 3);
-}
+
+	public void calculateWage() {
+		int workedHour = 0, totalWage = 0, totalWorkingHour = 0, totalDays = 0;
+		while (totalWorkingHour < 100 && totalDays < 20) {
+			int attendence = attendence();
+			switch (attendence) {
+			case 0:
+				workedHour = 0;
+				break;
+			case 1:
+				workedHour = fullTimeHour;
+				break;
+			case 2:
+				workedHour = partTimeHour;
+				break;
+			default:
+				System.out.println("error");
+			}
+			totalWorkingHour = totalWorkingHour + workedHour;
+			totalDays++;
+			totalWage = totalWage + wagePerHour * workedHour * attendence;
+		}
+		System.out.println("emp wage of " + this.companyName + " is " + totalWage);
+	}
+
+	public int attendence() {
+		return (int) Math.floor((Math.random() * 10) % 3);
+	}
+
+	public int getWagePerHour() {
+		return wagePerHour;
+	}
+
+	public void setWagePerHour(int wagePerHour) {
+		this.wagePerHour = wagePerHour;
+	}
+
+	public int getFullTimeHour() {
+		return fullTimeHour;
+	}
+
+	public void setfullTimeHour(int fullTimeHour) {
+		this.fullTimeHour = fullTimeHour;
+	}
+
+	public int getPartTimeHour() {
+		return partTimeHour;
+	}
+
+	public void setpartTimeHour(int partTimeHour) {
+		this.partTimeHour = partTimeHour;
+	}
+
 }
